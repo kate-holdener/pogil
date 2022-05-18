54b9ce877d4034a8612b2361936f4a5c36fec81d
Kate Holdener <kate.holdener@slu.edu>
2022-05-18 15:48:38 -0500

    adjusted horizontal spacing

--- software_design/clean_code/modelB/Deck.java
+++ software_design/clean_code/modelB/Deck.java
@@ -8,31 +8,31 @@ public class Deck
 
    public Deck()
    {
-       cards = new ArrayList<Card>();
-       for (Card.Face face: Card.Face.values())
-       {
-           cards.add(new Card(face, Card.Suite.SPADES));
-           cards.add(new Card(face, Card.Suite.HEARTS));
-           cards.add(new Card(face, Card.Suite.CLUBS));
-           cards.add(new Card(face, Card.Suite.DIAMONDS));
-       }
-       randomizer = new Random();
+      cards = new ArrayList<Card>();
+      for (Card.Face face: Card.Face.values())
+      {
+          cards.add(new Card(face, Card.Suite.SPADES));
+          cards.add(new Card(face, Card.Suite.HEARTS));
+          cards.add(new Card(face, Card.Suite.CLUBS));
+          cards.add(new Card(face, Card.Suite.DIAMONDS));
+      }
+      randomizer = new Random();
    }
 
    public void shuffle()
    {
-       for (int i = 0; i < cards.size(); i++)
-       {
-           int swap_with = randomizer.nextInt(cards.size());
-           Card a = cards.get(i);
-           cards.set(i, cards.get(swap_with));
-           cards.set(swap_with, a);
-       }
+      for (int i = 0; i < cards.size(); i++)
+      {
+         int swap_with = randomizer.nextInt(cards.size());
+         Card a = cards.get(i);
+         cards.set(i, cards.get(swap_with));
+         cards.set(swap_with, a);
+      }
    }
 
    public Card getTopCard()
    {
-       Card c = cards.remove(cards.size()-1);
-       return c;
+      Card c = cards.remove(cards.size()-1);
+      return c;
    }
 }
