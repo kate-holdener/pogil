1d233be70ef3a39a7549a4d9c3844b6cd1b74067
Kate Holdener <kate.holdener@slu.edu>
2022-05-19 11:01:20 -0500

    made more vertical spacing modifications

--- software_design/beautiful_code/modelA/Deck.java
+++ software_design/beautiful_code/modelA/Deck.java
@@ -1,14 +1,12 @@
 import java.util.ArrayList;
 import java.util.Random;
 public class Deck
-{
-   private ArrayList<Card> x;
+{  private ArrayList<Card> x;
    private Random y;
 public void rand()
 {
     for (int i = 0; i < x.size(); i++)
-    {
-        int swap_with = y.nextInt(x.size());
+    {   int swap_with = y.nextInt(x.size());
         Card a = x.get(i);
         x.set(i, x.get(swap_with));
         x.set(swap_with, a);
@@ -41,7 +39,6 @@ public void rand()
    public Card card()
    {
        Card c = x.remove(x.size()-1);
-   return c;
-   }
+   return c;}
 
 }
